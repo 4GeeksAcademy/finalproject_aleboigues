7e925724c746f1ba34df46b373a0b6d5e7d87a31
Alejandrabalv <48587816+Alejandrabalv@users.noreply.github.com>
2024-06-26 08:13:38 +0000

    character

--- src/api/models.py
+++ src/api/models.py
@@ -32,6 +32,23 @@ class Favorite(db.Model):
             "user_id": self.user_id,
             "item_id": self.item_id,
         }
+    
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    specie = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
 
 
   
