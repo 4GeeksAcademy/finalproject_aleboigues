520650e198c35290b7fdade76e31071c1b72e2ad
Alejandrabalv <48587816+Alejandrabalv@users.noreply.github.com>
2024-07-05 08:18:21 +0000

    frontend characters

--- src/front/js/pages/Characters.js
+++ src/front/js/pages/Characters.js
@@ -1,42 +1,48 @@
 import React, { useEffect, useState } from "react";
 
- const Characters = () => {
+const Characters = () => {
 
- const [characters, setCharacters] = useState ([]);
+   const [characters, setCharacters] = useState([]);
 
- async function ejecutarApiExterna (){
-    const response = await fetch("https://improved-space-meme-qpxxp9766r9c44qp-3001.app.github.dev/api/apiexterna", {method:"POST", headers:{"Content-Type":"application/json"}}); 
-    const data = await response.jscon();
-};
+   async function ejecutarApiExterna() {
+      const response = await fetch("https://improved-space-meme-qpxxp9766r9c44qp-3001.app.github.dev/api/apiexterna", { method: "POST", headers: { "Content-Type": "application/json" } });
+      const data = await response.jscon();
+   };
 
- async function traerPersonajes (){
-    const response = await fetch("https://improved-space-meme-qpxxp9766r9c44qp-3001.app.github.dev/api/characters", {method:"GET"});
-    const data = await response.json();
-    setCharacters(data);
- };
+   async function traerPersonajes() {
+      const response = await fetch("https://improved-space-meme-qpxxp9766r9c44qp-3001.app.github.dev/api/characters", { method: "GET" });
+      const data = await response.json();
+      setCharacters(data);
+   };
 
- useEffect(()=>{
-    ejecutarApiExterna();
-    traerPersonajes();
+   useEffect(() => {
+      ejecutarApiExterna();
+      traerPersonajes();
 
- }, [])
+   }, [])
 
-	return ( 
-        
-        <>
-        <h1>Personajes Rick N Morty</h1>
-        {characters.map(item => ( 
-            <>
-            <img src={item.image}></img>
-            <h2>{item.name}</h2>
-            <p>Especie:{item.species}</p>
-            <p>Estado:{item.status}</p>
-            <p>Género:{item.gender}</p>
-           </>
-        ))}
-        </>
-		
-	);
+   return (
+
+      <div className="container">
+         <h1 className="text-center my-4">Personajes de Rick and Morty</h1>
+         <div className="row">
+            {characters.map((character, index) => (
+               <div key={index} className="col-lg-4 col-md-6 mb-4">
+                  <div className="card h-100">
+                     <img src={character.image} className="card-img-top" alt={character.name} />
+                     <div className="card-body">
+                        <h5 className="card-title">{character.name}</h5>
+                        <p className="card-text">Especie: {character.species}</p>
+                        <p className="card-text">Estado: {character.status}</p>
+                        <p className="card-text">Género: {character.gender}</p>
+                     </div>
+                  </div>
+               </div>
+            ))}
+         </div>
+      </div>
+
+   );
 };
 
 
