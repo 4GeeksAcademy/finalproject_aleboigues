c59cf900d64ddcb6903c4cc24f921f9d7f071eb7
Alejandrabalv <48587816+Alejandrabalv@users.noreply.github.com>
2024-07-15 11:08:54 +0000

    front main page

--- src/front/js/component/MainPage.js
+++ src/front/js/component/MainPage.js
@@ -1,27 +1,31 @@
 import React from "react";
+import "/workspaces/finalproject_aleboigues/src/front/styles/mainpage.css"; // Importa tu archivo CSS aquí
 
 const MainPage = () => {
     return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-8 text-center">
-                    <h1 className="display-4">Bienvenido al Universo de Rick y Morty</h1>
-                    <p className="lead mt-4">
-                        Sumérgete
-                        en el increíble y caótico universo de Rick y Morty. Aquí encontrarás
-                        información detallada sobre todos los personajes de la serie.
-                    </p>
-                    <p className="mt-4">
-                        Por
-                        favor, regístrate o inicia sesión para acceder a la lista completa de
-                        personajes y descubrir más sobre sus aventuras interdimensionales.
-                    </p>
-                    <div className="mt-5">
-                        <img
-                            src="https://via.placeholder.com/600x300"
-                            alt="Rick and Morty Banner"
-                            className="img-fluid rounded shadow-sm"
-                        />
+        <div className="main-page">
+            <video autoPlay loop muted className="video-bg">
+                <source src="tu_video.mp4" type="video/mp4" />
+                Tu navegador no soporta videos HTML5.
+            </video>
+
+            <div className="container mt-5 text-center text-white">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <h1 className="display-4">Bienvenido al Universo de Rick y Morty</h1>
+                        <p className="lead mt-4">
+                            Sumérgete en el increíble y caótico universo de Rick y Morty. Aquí encontrarás información detallada sobre todos los personajes de la serie.
+                        </p>
+                        <p className="mt-4">
+                            Por favor, regístrate o inicia sesión para acceder a la lista completa de personajes y descubrir más sobre sus aventuras interdimensionales.
+                        </p>
+                       {/*   <div className="mt-5">
+                            <img
+                                src="https://via.placeholder.com/600x300"
+                                alt="Rick and Morty Banner"
+                                className="img-fluid rounded shadow-sm"
+                            />
+                        </div>*/}
                     </div>
                 </div>
             </div>
