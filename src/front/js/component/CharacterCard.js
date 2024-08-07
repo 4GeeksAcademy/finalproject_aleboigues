8fbde8effa3dc27f00a2fabed0aee0d0001ff5e8
Ale <48587816+Alejandrabalv@users.noreply.github.com>
2024-08-07 18:53:28 +0000

    botón learn more

--- src/front/js/component/CharacterCard.js
+++ src/front/js/component/CharacterCard.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AddFavoriteButton from './AddFavoriteButton';
 
 const CharacterCard = ({ character, token }) => {
     return (
         <div className="card h-100">
             <img src={character.image} className="card-img-top" alt={character.name} />
-            <div className="card-body">
-                <h5 className="card-title">{character.name}</h5>
-                <AddFavoriteButton characterId={character.id} token={token} />
-            </div>
+           
         </div>
     );
 };
